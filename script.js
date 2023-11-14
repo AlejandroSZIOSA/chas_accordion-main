70b3550e0a9fd3c2948193a8d0d0bf6518769e84
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-14 09:09:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// A function that adds and remove the class "active" on the section you click on.
+function toggle(e) {
+  const element = e.target;
+  element.classList.toggle("active");
+}
+
+// Selects and HTML element, and calls a function which will be executed when the element is clicked.
+const section1Element = document.getElementById("section1");
+section1Element.addEventListener("click", toggle);
