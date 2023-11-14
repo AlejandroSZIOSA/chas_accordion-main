e2fd80e53ce46e8b012415ed15ece9714b27b493
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-14 14:21:16 +0100

    Part one finish

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  const descriptionElement = document.querySelector(".description");
-  console.log(descriptionElement);
-
-  //descriptionElement.classList.add("active");
-
-  /* descriptionElement.setAttribute("display", "hidden"); */
-  descriptionElement.classList.toggle("active");
-  /* const element = e.target; */
-  /* element.classList.toggle("active"); */
-  console.log("toggle");
+function toggle(e) {
+  const element = e.target;
+  /* Toggle will add one class, the class must be defined in the Css stylesheet 
+  ,If not will create an empty class  */
+  element.classList.toggle("active");
 }
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-const section1Element = document.getElementById("section1");
-
-section1Element.addEventListener("click", toggle);
+const titleElements = document
+  .querySelectorAll(".title")
+  .forEach((titles) => titles.addEventListener("click", toggle));
