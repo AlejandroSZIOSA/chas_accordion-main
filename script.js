c06438bb2a661c2226570616857c1e2809144fb2
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-14 10:50:40 +0100

    hidden one description

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle(e) {
-  const element = e.target;
-  element.classList.toggle("active");
+function toggle() {
+  const descriptionElement = document.querySelector(".description");
+  console.log(descriptionElement);
+
+  //descriptionElement.classList.add("active");
+
+  /* descriptionElement.setAttribute("display", "hidden"); */
+  descriptionElement.classList.toggle("active");
+  /* const element = e.target; */
+  /* element.classList.toggle("active"); */
+  console.log("toggle");
 }
 
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
 const section1Element = document.getElementById("section1");
+
 section1Element.addEventListener("click", toggle);
