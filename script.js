961aea1326e62524e9e0db98efbd1fd16a36beec
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-15 15:32:46 +0100

    organize Part 2 start project

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle(e) {
-  const element = e.target;
-  /* Toggle will add one class, the class must be defined in the Css stylesheet 
-  ,If not will create an empty class  */
-  element.classList.toggle("active");
-}
-
-const titleElements = document
-  .querySelectorAll(".title")
-  .forEach((titles) => titles.addEventListener("click", toggle));
 
 async function getData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/");
@@ -18,9 +8,19 @@ async function getData() {
 
 function renderData(data) {
   console.log(data);
-
+  const titleElements = document
+    .querySelector("h1")
+    .forEach((titles) => titles.addEventListener("click", toggle));
   data.forEach((e) => {});
 }
 
-function createDivSection() {}
-getData();
+function toggle(e) {
+  const element = e.target;
+  /* Toggle will add one class, the class must be defined in the Css stylesheet 
+  ,If not will create an empty class  */
+  element.classList.toggle("active");
+}
+
+/* const titleElements = document
+  .querySelectorAll(".title")
+  .forEach((titles) => titles.addEventListener("click", toggle)); */
