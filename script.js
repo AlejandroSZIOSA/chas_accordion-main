5ec8ec060e321d6474873769899dec0b763bf037
AlejandroSZIOSA <ggato33@gmail.com>
2023-11-16 13:28:58 +0100

    Retouches

--- script.js
+++ script.js
@@ -6,37 +6,34 @@ async function getData() {
   renderData(data);
 }
 
-function renderData(data) {
-  console.log(data);
-  const header = document.querySelector("h1");
+getData();
 
-  data.forEach((el) => {
+function renderData(data) {
+  //Iterate on the fetched Data
+  data.forEach((item) => {
     //creating section title
     const titleEl = document.createElement("div");
     titleEl.setAttribute("class", "section");
-    titleEl.innerHTML = `<p>${el.title}</p>`;
+    titleEl.innerHTML = `<p>${item.title}</p>`;
 
     //creating section description
     const bodyEl = document.createElement("div");
     bodyEl.setAttribute("class", "description");
-    bodyEl.innerHTML = `<p>${el.body}</p>`;
+    bodyEl.innerHTML = `<p>${item.body}</p>`;
 
     //Adding section title to the html :)
     const mainSectionEl = document.querySelector(".accordion");
     mainSectionEl.appendChild(titleEl);
     mainSectionEl.appendChild(bodyEl);
 
+    //Calling EventListener and pass a function as param
     titleEl.addEventListener("click", toggle);
   });
 }
+
 function toggle(e) {
   const element = e.currentTarget; //Fix problem :)
   /* Toggle will add one class, the class must be defined in the Css stylesheet 
   ,If not will create an empty class  */
   element.classList.toggle("active");
-  console.log(element);
 }
-getData();
-/* const titleElements = document
-  .querySelectorAll(".title")
-  .forEach((titles) => titles.addEventListener("click", toggle)); */
